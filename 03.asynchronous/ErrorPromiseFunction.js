b28a6b5e7a8ce30f24c326d044cae0537c45d6be
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2024-02-20 23:59:09 +0900

    エラーファイルを2つに分ける必要はない

--- 03.asynchronous/ErrorPromiseFunction.js
+++ 03.asynchronous/ErrorPromiseFunction.js
@@ -2,15 +2,15 @@ import Table from "./commonQuery.js";
 
 const createTableQuery =
   "CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)";
-const errorInsertTableQuery = "INSERT INTO books(title) VALUES(NULL)";
-const GetRecordsQuery = "SELECT * FROM books";
+const insertTableQuery = "INSERT INTO books(title) VALUES(NULL)";
+const errorGetRecordsQuery = "SELECT undefined FROM books";
 const dropTableQuery = "DROP TABLE books";
 
 const db = new Table();
 db.run(createTableQuery)
-  .then(() => db.run(errorInsertTableQuery))
+  .then(() => db.run(insertTableQuery))
   .then(({ lastID }) => console.log(`自動採択されたID ${lastID}`))
-  .then(() => db.all(GetRecordsQuery))
+  .then(() => db.all(errorGetRecordsQuery))
   .then((rows) => db.display(rows))
   .then(() => db.run(dropTableQuery))
   .catch((e) => console.error(e));
