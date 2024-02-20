b4f45ab8b6a075314f853fe5ddcc3dc4c6c48229
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2024-02-20 21:42:59 +0900

    独自の関数を定義せずに、エラーあり・なしの2つのファイルを作成した

--- 03.asynchronous/ErrorCallbackFunction.js
+++ 03.asynchronous/ErrorCallbackFunction.js
@@ -1,22 +1,30 @@
 import sqlite3 from "sqlite3";
 const db = new sqlite3.Database(":memory:");
 
-const noErrorCallbackFunction = (db) => {
+const ErrorCallbackFunction = (db) => {
   const createTableQuery = "CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)";
   db.run(createTableQuery, () => {
-      const insertQuery = "INSERT INTO books(title) VALUES('OK')";
-      db.run(insertQuery, function() {
+      const insertQuery = "INSERT INTO books(title) VALUES(NULL)";
+      db.run(insertQuery, function(err) {
+        if(err) {
+          console.error(err);
+        } else {
           console.log(`自動採択されたID ${this.lastID}`);
-          const selectRecordsQuery = "SELECT * FROM books";
+          const selectRecordsQuery = "SELECT undefined FROM books";
           db.all(selectRecordsQuery, function(err, rows) {
+            if(err) {
+              console.error(err);
+            } else {
               for (const row of rows) {
                 console.log(`${row.id} ${row.title}`);
               }
               const dropQuery = "DROP TABLE books";
               db.run(dropQuery);
-          });
-      });
-  });
+            }
+          })
+        }
+      })
+  })
 };
 
-noErrorCallbackFunction(db);
+ErrorCallbackFunction(db);
