cdbebb75c4372880e2dc658dcc2350482128b414
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2024-02-27 18:00:59 +0900

    Top-level awaitを使う

--- 03.asynchronous/errorAsyncFunctoin.js
+++ 03.asynchronous/errorAsyncFunctoin.js
@@ -1,33 +1,29 @@
 import { run, all } from "./commonQuery.js";
 import sqlite3 from "sqlite3";
 
-const errorAsyncFunction = async function () {
-  const db = new sqlite3.Database(":memory:");
-  const createTableQuery =
-    "CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)";
-  const errorInsertTableQuery = "INSERT INTO books(title) VALUES(NULL)";
-  const errorGetRecordsQuery = "SELECT undefined FROM books";
-  const dropTableQuery = "DROP TABLE books";
-  const display = (rows) => {
-    for (const row of rows) {
-      console.log(`${row.id} ${row.title}`);
-    }
-  };
-
-  await run(db, createTableQuery);
-  try {
-    const { lastID } = await run(db, errorInsertTableQuery);
-    console.log(`自動採番されたID ${lastID}`);
-  } catch (err) {
-    console.log(err.message);
+const db = new sqlite3.Database(":memory:");
+const createTableQuery =
+  "CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)";
+const errorInsertTableQuery = "INSERT INTO books(title) VALUES(NULL)";
+const errorGetRecordsQuery = "SELECT undefined FROM books";
+const dropTableQuery = "DROP TABLE books";
+const display = (rows) => {
+  for (const row of rows) {
+    console.log(`${row.id} ${row.title}`);
   }
-  try {
-    const rows = await all(db, errorGetRecordsQuery);
-    await display(rows);
-  } catch (err) {
-    console.log(err.message);
-  }
-  await run(db, dropTableQuery);
 };
 
-errorAsyncFunction();
+await run(db, createTableQuery);
+try {
+  const { lastID } = await run(db, errorInsertTableQuery);
+  console.log(`自動採番されたID ${lastID}`);
+} catch (err) {
+  console.log(err.message);
+}
+try {
+  const rows = await all(db, errorGetRecordsQuery);
+  await display(rows);
+} catch (err) {
+  console.log(err.message);
+}
+await run(db, dropTableQuery);
--- 03.asynchronous/noErrorAsyncFunctoin.js
+++ 03.asynchronous/noErrorAsyncFunctoin.js
@@ -1,25 +1,21 @@
 import { run, all } from "./commonQuery.js";
 import sqlite3 from "sqlite3";
 
-const noErrorAsyncFunction = async function () {
-  const db = new sqlite3.Database(":memory:");
-  const createTableQuery =
-    "CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)";
-  const insertTableQuery = "INSERT INTO books(title) VALUES('吾輩は猫である')";
-  const getRecordsQuery = "SELECT * FROM books";
-  const dropTableQuery = "DROP TABLE books";
-  const display = (rows) => {
-    for (const row of rows) {
-      console.log(`${row.id} ${row.title}`);
-    }
-  };
-
-  await run(db, createTableQuery);
-  const { lastID } = await run(db, insertTableQuery);
-  console.log(`自動採番されたID ${lastID}`);
-  const rows = await all(db, getRecordsQuery);
-  await display(rows);
-  await run(db, dropTableQuery);
+const db = new sqlite3.Database(":memory:");
+const createTableQuery =
+  "CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)";
+const insertTableQuery = "INSERT INTO books(title) VALUES('吾輩は猫である')";
+const getRecordsQuery = "SELECT * FROM books";
+const dropTableQuery = "DROP TABLE books";
+const display = (rows) => {
+  for (const row of rows) {
+    console.log(`${row.id} ${row.title}`);
+  }
 };
 
-noErrorAsyncFunction();
+await run(db, createTableQuery);
+const { lastID } = await run(db, insertTableQuery);
+console.log(`自動採番されたID ${lastID}`);
+const rows = await all(db, getRecordsQuery);
+await display(rows);
+await run(db, dropTableQuery);
