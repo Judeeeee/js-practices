9fc8e1216b0b695e17b5af3202b57f81481d532f
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2024-02-27 11:10:08 +0900

    本のタイトルを指定した

--- 03.asynchronous/noErrorCallbackFunction.js
+++ 03.asynchronous/noErrorCallbackFunction.js
@@ -3,7 +3,7 @@ import sqlite3 from "sqlite3";
 const noErrorCallbackFunction = () => {
   const db = new sqlite3.Database(":memory:");
   db.run("CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)", () => {
-    db.run("INSERT INTO books(title) VALUES('OK')", function () {
+    db.run("INSERT INTO books(title) VALUES('吾輩は猫である')", function () {
       console.log(`自動採択されたID ${this.lastID}`);
       db.all("SELECT * FROM books", function (err, rows) {
         for (const row of rows) {
