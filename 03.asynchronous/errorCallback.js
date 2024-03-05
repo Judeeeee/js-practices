4737ae66aa2559e80c5cce34ed1abb717431f108
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2024-03-05 17:15:33 +0900

    関数自体が必要ないので削除

--- 03.asynchronous/errorCallback.js
+++ 03.asynchronous/errorCallback.js
@@ -1,29 +1,25 @@
 import sqlite3 from "sqlite3";
 
-const errorCallbackFunction = () => {
-  const db = new sqlite3.Database(":memory:");
-  db.run(
-    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-    () => {
-      db.run("INSERT INTO books(title) VALUES(NULL)", function (err) {
+const db = new sqlite3.Database(":memory:");
+db.run(
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  () => {
+    db.run("INSERT INTO books(title) VALUES(NULL)", function (err) {
+      if (err) {
+        console.log(err.message);
+      } else {
+        console.log(`自動採番されたID ${this.lastID}`);
+      }
+      db.all("SELECT undefined FROM books", (err, rows) => {
         if (err) {
           console.log(err.message);
         } else {
-          console.log(`自動採番されたID ${this.lastID}`);
-        }
-        db.all("SELECT undefined FROM books", (err, rows) => {
-          if (err) {
-            console.log(err.message);
-          } else {
-            for (const row of rows) {
-              console.log(`${row.id} ${row.title}`);
-            }
-            db.run("DROP TABLE books");
+          for (const row of rows) {
+            console.log(`${row.id} ${row.title}`);
           }
-        });
+          db.run("DROP TABLE books");
+        }
       });
-    },
-  );
-};
-
-errorCallbackFunction();
+    });
+  },
+);
--- 03.asynchronous/noErrorCallback.js
+++ 03.asynchronous/noErrorCallback.js
@@ -1,6 +1,5 @@
 import sqlite3 from "sqlite3";
 
-const noErrorCallbackFunction = () => {
   const db = new sqlite3.Database(":memory:");
   db.run(
     "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
@@ -16,6 +15,3 @@ const noErrorCallbackFunction = () => {
       });
     },
   );
-};
-
-noErrorCallbackFunction();
