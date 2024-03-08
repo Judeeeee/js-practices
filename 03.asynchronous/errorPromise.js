7a472991a0d168a00f92e4bdf9c52c588eebf132
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2024-03-08 17:52:37 +0900

    見やすさのためにonRejectedからcatchに変更

--- 03.asynchronous/errorPromise.js
+++ 03.asynchronous/errorPromise.js
@@ -5,30 +5,15 @@ const db = new sqlite3.Database(":memory:");
 
 run(db, "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)")
   .then(() => run(db, "INSERT INTO books(title) VALUES(NULL)"))
-  .then(
-    (lastID) => {
-      console.log(`自動採番されたID ${lastID}`);
-    },
-    (err) => {
-      console.log(err.message);
-    },
-  )
-  .then(
-    () => {
-      return all(db, "SELECT undefined FROM books");
-    },
-    (err) => {
-      return err;
-    },
-  )
+  .then((lastID) => { console.log(`自動採番されたID ${lastID}`);})
+  .catch((err) => {console.log(err.message);})
+  .then(() => { return all(db, "SELECT undefined FROM books");})
   .then(
     (rows) => {
       for (const row of rows) {
         console.log(`${row.id} ${row.title}`);
       }
-    },
-    (err) => {
-      console.log(err.message);
-    },
+    }
   )
+  .catch((err) => {console.log(err.message);})
   .then(() => run(db, "DROP TABLE books"));
