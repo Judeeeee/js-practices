4c4f146f64b324216513d3d0826d13f527f9163b
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2024-03-26 10:58:39 +0900

    不要なawaitの削除

--- 04.class/Main.js
+++ 04.class/Main.js
@@ -37,7 +37,7 @@ const Main = async function () {
       }
     }
   } else {
-    await process.stdin.on("data", (data) => {
+    process.stdin.on("data", (data) => {
       const text = data.toString().trim();
       Record.insert(db, text);
     });
