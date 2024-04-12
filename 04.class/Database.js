7d1f691d0f2d5c9a839a102ec21765a722e41244
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2024-04-12 15:59:56 +0900

    staticからconstructorを使う形に変更
    わざわざstaticにする必要がないため

--- 04.class/Database.js
+++ 04.class/Database.js
@@ -1,11 +1,13 @@
 import sqlite3 from "sqlite3";
 
 export default class Database {
-  static db = new sqlite3.Database("./memo.db");
+  constructor() {
+    this.db = new sqlite3.Database("./memo.db");
+  }
 
   initialize() {
     return new Promise((resolve, reject) => {
-      Database.db.run(
+      this.db.run(
         "CREATE TABLE IF NOT EXISTS memos(id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT NOT NULL)",
         (err) => {
           if (err) {
@@ -20,24 +22,21 @@ export default class Database {
 
   selectAll() {
     return new Promise((resolve, reject) => {
-      Database.db.all(
-        "SELECT * FROM memos ORDER BY id ASC",
-        function (err, rows) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(rows);
-          }
-        },
-      );
+      this.db.all("SELECT * FROM memos ORDER BY id ASC", function (err, rows) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
     });
   }
 
   insert(text) {
-    Database.db.run("INSERT INTO memos(text) VALUES(?)", text);
+    this.db.run("INSERT INTO memos(text) VALUES(?)", text);
   }
 
   delete(memoID) {
-    Database.db.run("DELETE FROM memos WHERE id = ?", memoID);
+    this.db.run("DELETE FROM memos WHERE id = ?", memoID);
   }
 }
