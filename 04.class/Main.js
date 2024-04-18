05aa3b661f624355c6b6c10ba1f96a82cc02b39c
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2024-04-18 15:37:30 +0900

    指定オプションの有無で条件分岐させていることを明示
    rとdオプションはメモが保存されていない場合に実行するとエラーになる。
    そのため、更にmemos.lengthの条件分岐を入れている。

--- 04.class/Main.js
+++ 04.class/Main.js
@@ -11,7 +11,32 @@ const main = async function () {
   const memoApp = new MemoApp(memos);
   const option = process.argv[2];
 
-  if (option === undefined) {
+  if (option === "-l") {
+    const titles = memoApp.createTitles();
+    for (const title of titles) {
+      console.log(title);
+    }
+  } else if (option === "-r") {
+    const request_message = "Choose a memo you want to see";
+    if (memos.length !== 0) {
+      try {
+        const chosenMemo = await memoApp.choose(request_message);
+        console.log(chosenMemo.text);
+      } catch {
+        return;
+      }
+    }
+  } else if (option === "-d") {
+    const request_message = "Choose a memo you want to delete";
+    if (memos.length !== 0) {
+      try {
+        const chosenMemo = await memoApp.choose(request_message);
+        await memosDatabase.delete(chosenMemo.id);
+      } catch {
+        return;
+      }
+    }
+  } else if (option === undefined) {
     const lines = [];
     const reader = readline.createInterface({
       input: process.stdin,
@@ -25,36 +50,6 @@ const main = async function () {
     reader.on("close", async () => {
       await memosDatabase.insert(lines.join("\n"));
     });
-  } else if (memos.length !== 0) {
-    switch (option) {
-      case "-l": {
-        const titles = memoApp.createTitles();
-        for (const title of titles) {
-          console.log(title);
-        }
-        break;
-      }
-      case "-r": {
-        const request_message = "Choose a memo you want to see";
-        try {
-          const chosenMemo = await memoApp.choose(request_message);
-          console.log(chosenMemo.text);
-        } catch {
-          return;
-        }
-        break;
-      }
-      case "-d": {
-        const request_message = "Choose a memo you want to delete";
-        try {
-          const chosenMemo = await memoApp.choose(request_message);
-          await memosDatabase.delete(chosenMemo.id);
-        } catch {
-          return;
-        }
-        break;
-      }
-    }
   }
 };
 
